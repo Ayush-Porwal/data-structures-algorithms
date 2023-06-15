ea8121ef816cb75c3f168838eb98eae9f146947b
Ayush Porwal <aporwal@concertai.com>
2023-06-15 18:06:02 +0530

    adding stack in js

--- JS/stack.js
+++ JS/stack.js
@@ -1,1 +1,25 @@
-class Stack {}
+export class Stack {
+    constructor() {
+        this.stack = [];
+    }
+
+    isEmpty() {
+        return this.stack.length === 0;
+    }
+
+    top() {
+        return this.stack[this.stack.length - 1];
+    }
+
+    size() {
+        return this.stack.length;
+    }
+
+    push(value) {
+        this.stack.push(value);
+    }
+
+    pop(value) {
+        this.stack.pop(value);
+    }
+}
